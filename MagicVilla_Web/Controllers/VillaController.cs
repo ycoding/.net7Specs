ac9fc4560c88bd2d3422be618139b29240cc6074
Younes Hashlamoun <Younes.Engineer@Gmail.com>
2022-12-07 22:14:12 -0600

    Create Villa View Listing All Villas

--- MagicVilla_Web/Controllers/VillaController.cs
+++ MagicVilla_Web/Controllers/VillaController.cs
@@ -4,6 +4,7 @@ using MagicVilla_Web.Models.Dto;
 using MagicVilla_Web.Services.IServices;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace MagicVilla_Web.Controllers
 {
@@ -26,5 +27,25 @@ namespace MagicVilla_Web.Controllers
             }
             return View(list);
         }
-    }
+
+		public async Task<IActionResult> CreateVilla()
+		{
+			return View();
+		}
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+		public async Task<IActionResult> CreateVilla(VillaCreateDTO model)
+		{
+			if(ModelState.IsValid)
+            {
+				var response = await _villaService.CreateAsync<APIResponse>(model);
+				if (response != null && response.IsSuccess)
+                {
+                    return RedirectToAction(nameof(IndexVilla));
+                }
+			}
+			return View(model);
+		}
+	}
 }
